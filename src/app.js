b9e76064c63dde971912a6e98fbf87e70eac129d
Winston J Lamus <138252771+WinstonLesuT@users.noreply.github.com>
2024-04-26 01:25:34 +0000

    boton added

--- src/app.js
+++ src/app.js
@@ -2,38 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
+let generateExcuse = function() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoindx = Math.floor(Math.random() * who.length);
+  let actindx = Math.floor(Math.random() * action.length);
+  let whatindx = Math.floor(Math.random() * what.length);
+  let whenindx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindx] +
+    " " +
+    action[actindx] +
+    " " +
+    what[whatindx] +
+    " " +
+    when[whenindx] +
+    " "
+  );
+};
 
 window.onload = function() {
-  let generateExcuse = function() {
-    let who = ["The dog", "My grandma", "His turtle", "My bird"];
-    let action = ["ate", "peed", "crushed", "broke"];
-    let what = ["my homework", "the keys", "the car"];
-    let when = [
-      "before the class",
-      "right on time",
-      "when I finished",
-      "during my lunch",
-      "while I was praying"
-    ];
-
-    let whoindx = Math.floor(Math.random() * who.length);
-    let actindx = Math.floor(Math.random() * action.length);
-    let whatindx = Math.floor(Math.random() * what.length);
-    let whenindx = Math.floor(Math.random() * when.length);
-
-    return (
-      who[whoindx] +
-      " " +
-      action[actindx] +
-      " " +
-      what[whatindx] +
-      " " +
-      when[whenindx] +
-      " "
-    );
-  };
-  document.querySelector("#excuse").innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
 };
